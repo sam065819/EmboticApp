be6276455dabca0b4e6ac773209fcb647dc89a88
sam065819 <sam065819@outlook.com>
2024-12-25 22:42:05 +0100

    Going from scratch

--- app/src/main/java/com/genuineinsanity/embotic/MainActivity.kt
+++ app/src/main/java/com/genuineinsanity/embotic/MainActivity.kt
@@ -5,42 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.view.View
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
-
 
 class MainActivity : AppCompatActivity() {
-    lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView = findViewById(R.id.webView)
-        webView.setInitialScale(1)
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.useWideViewPort = true
-        webView.scrollBarStyle = WebView.SCROLLBARS_INSIDE_OVERLAY
-        webView.isScrollbarFadingEnabled = true
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.allowContentAccess = true
-        webView.settings.allowFileAccess = true
-        webView.settings.builtInZoomControls = true
-        webView.settings.displayZoomControls = false
-        webView.settings.setSupportZoom(true)
-        webView.isHorizontalScrollBarEnabled = false
-        webView.isClickable = true
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(
-                view: WebView,
-                url: String
-            ): Boolean {
-                view?.loadUrl(url)
-                return true
-            }
-    }
-        webView.loadUrl("https://dash.embotic.xyz")
 
     }
 }
