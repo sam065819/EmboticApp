4ef829461d52a8b6c4525164c1fd62b0ca7bb739
sam065819 <sam065819@outlook.com>
2024-12-25 21:47:57 +0100

    everything

---
+++ app/src/main/java/com/genuineinsanity/embotic/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.genuineinsanity.embotic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
