dff0d54aa2eb7bfebbf37a0c59e97e7b1c6b1fbd
sam065819 <sam065819@outlook.com>
2024-12-29 14:53:44 +0100

    2 issues were found when checking AAR metadata:
      1.  Dependency 'androidx.core:core:1.15.0' requires libraries and applications that
          depend on it to compile against version 35 or later of the
          Android APIs.
    
          :app is currently compiled against android-34.
    
          Recommended action: Update this project to use a newer compileSdk
          of at least 35, for example 35.
    
          Note that updating a library or application's compileSdk (which
          allows newer APIs to be used) can be done separately from updating
          targetSdk (which opts the app in to new runtime behavior) and
          minSdk (which determines which devices the app can be installed
          on).
    
      2.  Dependency 'androidx.core:core-ktx:1.15.0' requires libraries and applications that
          depend on it to compile against version 35 or later of the
          Android APIs.
    
          :app is currently compiled against android-34.
    
          Recommended action: Update this project to use a newer compileSdk
          of at least 35, for example 35.
    
          Note that updating a library or application's compileSdk (which
          allows newer APIs to be used) can be done separately from updating
          targetSdk (which opts the app in to new runtime behavior) and
          minSdk (which determines which devices the app can be installed
          on).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.genuineinsanity.embotic"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 2
         versionName = "v1.1-beta"
 
